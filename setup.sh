609b8f34f3fe8c33248d1221e1c7ab469c4c874b
Chris Barker <bluquar@gmail.com>
2021-12-14 22:29:13 -0800

    test

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@
 export EDITOR=emacs
 
 # https://scriptim.github.io/bash-prompt-generator/
-export PS1='\[\e[0m\][\[\e[0;38;5;119m\]$?\[\e[0m\]] \[\e[0m\]%\[\e[0m\]\j \[\e[0m\](\[\e[0m\]\D{}\[\e[0m\]) \[\e[0;38;5;117m\]$(git branch 2>/dev/null | grep '"'"'^*'"'"' | colrm 1 2) \[\e[0;38;5;206m\]\W \[\e[0m\]> \[\e[0m\]'
+PS1='\[\e[0m\][\[\e[0;38;5;119m\]$?\[\e[0m\]] \[\e[0m\]%\[\e[0m\]\j \[\e[0m\](\[\e[0m\]\D{}\[\e[0m\]) \[\e[0;38;5;117m\]$(git branch 2>/dev/null | grep '"'"'^*'"'"' | colrm 1 2) \[\e[0;38;5;206m\]\W \[\e[0m\]> \[\e[0m\]'
